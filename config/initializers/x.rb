4a561200822772b7dd3b657440baa282f7fa2441
TakenakaEri <s2070818@s.do-johodai.ac.jp>
2024-05-29 12:19:15 +0900

    x.rbの追加

---
+++ config/initializers/x.rb
@@ -0,0 +1,10 @@
+# 初期化用
+
+require 'x'
+
+X_CREDENTIALS = {
+  api_key: ENV['TWITTER_API_KEY'],
+  api_key_secret: ENV['TWITTER_API_SECRET'],
+  access_token: ENV['TWITTER_ACCESS_TOKEN'],
+  access_token_secret: ENV['TWITTER_ACCESS_TOKEN_SECRET']
+}
