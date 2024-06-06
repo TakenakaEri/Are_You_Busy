dded34c748e9d32a119598915e2d3b11afcc966f
TakenakaEri <s2070818@s.do-johodai.ac.jp>
2024-06-06 09:10:37 +0900

    アソシエーションの変更

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
     belongs_to :user
-    belongs_to :tweet, foreign_key: 'twitter_tweet_id', primary_key: 'twitter_tweet_id'
+    belongs_to :tweet
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet < ApplicationRecord
     belongs_to :user
 
-    has_many :likes, foreign_key: 'twitter_tweet_id'
+    has_many :likes
     has_many :bookmarks, dependent: :destroy
 end
